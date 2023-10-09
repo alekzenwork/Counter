0c840453a2c9e91aabf3e820f056173ad775e145
alekzenwork <alekzenmusic@gmail.com>
2023-10-09 18:38:30 +0300

    Исправил значение UILabel, чтобы отображалась не только цифра, а "Значение счетчика: ..."

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var mainCounter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    private var number = 0
+    @IBAction func plus1(_ sender: Any) {
+        number += 1
+        mainCounter.text = "Значение счетчика: \(number)"
+    }
+    
 }
 
